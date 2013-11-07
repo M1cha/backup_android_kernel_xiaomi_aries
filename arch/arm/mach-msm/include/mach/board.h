176779d31e215453761773b36dd46db836a0e32d
M1cha <sigmaepsilon92@gmail.com>
2013-11-07 16:22:03 +0100

    aries: add fields for mipi_hitachi lcd driver

--- arch/arm/mach-msm/include/mach/board.h
+++ arch/arm/mach-msm/include/mach/board.h
@@ -394,6 +394,16 @@ struct msm_panel_common_pdata {
 	struct msm_bus_scale_pdata *mdp_bus_scale_table;
 #endif
 	int mdp_rev;
+	void *power_on_set_1;
+	void *power_on_set_2;
+	void *power_on_set_3;
+	ssize_t power_on_set_size_1;
+	ssize_t power_on_set_size_2;
+	ssize_t power_on_set_size_3;
+	void *power_off_set_1;
+	void *power_off_set_2;
+	ssize_t power_off_set_size_1;
+	ssize_t power_off_set_size_2;
 	u32 ov0_wb_size;  /* overlay0 writeback size */
 	u32 ov1_wb_size;  /* overlay1 writeback size */
 	u32 mem_hid;
@@ -401,6 +411,8 @@ struct msm_panel_common_pdata {
 	u32 splash_screen_addr;
 	u32 splash_screen_size;
 	char mdp_iommu_split_domain;
+	void (*bl_pwm_disable)(void);
+	int (*bl_on_status)(void);
 };
 
 
