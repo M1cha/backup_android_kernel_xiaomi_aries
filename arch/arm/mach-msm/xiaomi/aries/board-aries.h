1d2e08e6e0e0437f93965e309eff69e7d14ab2c1
M1cha <sigmaepsilon92@gmail.com>
2013-11-07 16:20:40 +0100

    aries: fix board header define and move i2c_registry to board_aries.h

--- arch/arm/mach-msm/xiaomi/aries/board-aries.h
+++ arch/arm/mach-msm/xiaomi/aries/board-aries.h
@@ -10,8 +10,8 @@
  * GNU General Public License for more details.
  */
 
-#ifndef __ARCH_ARM_MACH_MSM_BOARD_APQ8064_H
-#define __ARCH_ARM_MACH_MSM_BOARD_APQ8064_H
+#ifndef __ARCH_ARM_MACH_MSM_BOARD_APQ8064_ARIES_H
+#define __ARCH_ARM_MACH_MSM_BOARD_APQ8064_ARIES_H
 
 #include <linux/regulator/msm-gpio-regulator.h>
 #include <linux/mfd/pm8xxx/pm8921.h>
@@ -149,4 +149,20 @@ enum {
 
 extern struct msm_rtb_platform_data apq8064_rtb_pdata;
 extern struct msm_cache_dump_platform_data apq8064_cache_dump_pdata;
+
+#define I2C_SURF 1
+#define I2C_FFA  (1 << 1)
+#define I2C_RUMI (1 << 2)
+#define I2C_SIM  (1 << 3)
+#define I2C_LIQUID (1 << 4)
+#define I2C_MPQ_CDP BIT(5)
+#define I2C_MPQ_HRD BIT(6)
+#define I2C_MPQ_DTV BIT(7)
+
+struct i2c_registry {
+	u8                     machs;
+	int                    bus;
+	struct i2c_board_info *info;
+	int                    len;
+};
 #endif
